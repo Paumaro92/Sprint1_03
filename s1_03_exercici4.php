4c5f5639f56fe9275a9985b6f46c556b7c193e6b
Paumaro92 <paumr92@hotmail.es>
2025-05-15 13:45:27 +0200

    Exercici 4 final

---
+++ s1_03_exercici4.php
@@ -0,0 +1,13 @@
+<?php
+$infoMeva = array(
+    "nom" => "Pau",
+    "edat" => "32",
+    "email" => "paumr92@gmail.com",
+    "menjar favorit" => "pizza", 
+);
+echo "El meu nom és:" . $infoMeva["nom"] . "\n";
+echo "Tinc " .  $infoMeva["edat"] . " anys". "\n";
+echo "El meu email és:" . $infoMeva["email"]. "\n";
+echo "El meu menjar favorit és:" . $infoMeva["menjar favorit"]. "\n";
+
+?>
