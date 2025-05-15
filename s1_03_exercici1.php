a0ff71bfa9e65a2853c972c7782e0afc7231dc5a
Paumaro92 <paumr92@hotmail.es>
2025-05-15 12:45:40 +0200

    Exercici 1 finalitzat

---
+++ s1_03_exercici1.php
@@ -0,0 +1,14 @@
+<?php
+$nombresEnters =  [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5'
+];
+echo $nombresEnters[0] . "\n";
+echo $nombresEnters[1] . "\n";
+echo $nombresEnters[2] . "\n";
+echo $nombresEnters[3] . "\n";
+echo $nombresEnters[4] . "\n";
+?>
